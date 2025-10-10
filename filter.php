27902490e98d827a2dfccc3569e151ebd186b2b8
Iñigo Zendegi <izendegi@mondragon.edu>
2025-10-10 13:04:57 +0200

    Adapt to MDL-82427

--- filter.php
+++ filter.php
@@ -22,49 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-/**
- * The filter_gitquotes class.
- *
- * This class extends the moodle_text_filter class and is used to
- * filter the text and replace the custom quotes with the git-style quotes.
- */
-class filter_gitquotes extends moodle_text_filter {
-    /**
-     * The pattern for the custom quotes.
-     */
-    public const PATTERN_HTML = '/<blockquote>\s*?<p>\s*\[\!(NOTE|WARNING|TIP|IMPORTANT)\](.*?)<\/p>\s*<\/blockquote>/is';
-
-    /**
-     * Filter function.
-     * Filters the text and replaces the custom quotes with the git-style quotes.
-     *
-     * @param string $text The text to be filtered.
-     * @param array $options The options for the filter.
-     */
-    public function filter($text, array $options = []) {
-        if (empty($text)) {
-            return $text;
-        }
-
-        // Get the format of the text.
-        $format = "";
-        if (isset($options['context']) && method_exists($options['context'], 'get_format')) {
-            $format = $options['context']->get_format();
-        } else {
-            $format = FORMAT_MARKDOWN;
-        }
-
-        // If the format is not markdown, return the text as it is.
-        if (!in_array($format, [FORMAT_MARKDOWN])) {
-            return $text;
-        }
-
-        // Replace the custom quotes with the git-style quotes.
-        $replacementhtml = '<blockquote class="gitquote $1"><div class="gitquote_name">';
-        $replacementhtml .= '<strong class="quote-type $1">$1</strong></div><span class="quote-content">$2</span></blockquote>';
-
-        $text = preg_replace(self::PATTERN_HTML, $replacementhtml, $text);
-
-        return $text;
-    }
-}
+// The actual implementation is under classes/text_filter.php
+// This file is just a workaround to make the filter work
+// in Moodle versions below 4.5.
+class_alias(\filter_gitquotes\text_filter::class, \filter_gitquotes::class);
